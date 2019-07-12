7202c2e4a6a9a13a04b115334142ae6a63297287
Dinda Aditama <dinda.aditamaa@gmail.com>
2019-07-12 15:33:39 +0700

    career

---
+++ protected/views/home/career.php
@@ -0,0 +1,78 @@
+<section class="cover-insides">
+    <div class="begrond">
+        <div class="prelative container">
+                <div class="teks">
+                    <div class="row">
+                        <div class="col-md-60">
+                            <h1 class="teks-cover">Tentang Kami</h1>
+                        </div>
+                        <div class="col-md-60 ">
+                            <div class="teks">Sebuah wajah baru di dunia perdagangan besi bekas di Indonesia</div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
+</section>
+
+<section class="career-sec-2">
+    <div class="prelative container">
+        <div class="title-head">
+            <div class="avail">
+                <p>POSISI TERSEDIA DI PT. OPMS</p>
+            </div>
+        </div>
+
+                <div class="hr-career"></div>
+                <div class="title-pos">
+                    <p>Sales Executive
+                    </p>
+                </div>
+                <div class="row w-100 pb-5">
+                    <div class="col-md-20">
+                        <div class="desc pr-3"><p>It’s our diversified advisory experience that we want you to help us deliver and evolve. In doing so you’ll not just be at the forefront of the industry, you’ll be helping to drive its direction. We have a fantastic opportunity available for a switched on Client Adviser (Senior) in our Business Services division.</p>
+                        </div>
+                        <div class="work">
+                            <p>Work Locations: </p>
+                            <p>Surabaya</p>
+                        </div>
+                        <div class="work">
+                            <p>Email to apply for this job:</p>
+                            <p>career@corpus.co.id</p>
+                        </div>
+                    </div>
+                    <div class="col-md-20">
+                        <div class="heading-col">
+                            <p>job description</p>
+                            
+                        </div>
+                        <div class="req">
+                            <ul>
+                                <li>Maintain the existing global email solution in our international organisation (12000 accounts worldwide)</li>
+                                <li>Be the main point of contact for messaging related topics</li>
+                                <li>Coordinate integration between Givaudan messaging systems and other platforms (e.g.: CRM, home grown solutions, SAP, etc.)</li>
+                                <li>Leverage, design and implement evolution in messaging solutions as part of large projects</li>
+                                <li>Ensure smooth deployment and handover to operations (managed services partner)</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-20">
+                        <div class="heading-col">
+                            <p>qualifications</p>
+                        </div>
+                        <div class="req">
+                            <ul>
+                                <li>University Degree in IT or related field</li>
+                                <li>Minimum 7 years’ experience ideally with strong experience in </li>
+                                <li>Google messaging systems.</li>
+                                <li>Ability to analyze complex requirements, recommend and implement deployment strategies</li>
+                                <li>Ensure implemented solutions abide by security standards</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                    <div class="clear"></div>
+
+    </div>
+    <div class="pb-5"></div>
+</section>
