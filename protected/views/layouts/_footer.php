d7cd0bd414033f81c9f4740217917bb682533b02
Pandu Bagoes Pradana <pandubaguspr@gmail.com>
2019-07-12 13:01:15 +0700

    footer

--- protected/views/layouts/_footer.php
+++ protected/views/layouts/_footer.php
@@ -1,42 +1,17 @@
 <section class="footer">
-	<div class="prelative container">
-		<div class="row">
-			<div class="col-md-60">
-				<div class="box-title">
-					<div class="image"><img src="<?php echo $this->assetBaseurl; ?>Layer-18-copy.png" alt=""></div>
-					<div class="title">
-						<p>PT. ANGKASARAYA STEEL</p>
-					</div>
-					<div class="subtitle">
-						<p>Working towards a world that lasts forever</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-60">
-				<div class="box-content">
-					<div class="alamat">
-						<p>GRESIK FACTORY - SURABAYA</p>
-					</div>
-					<div class="jalan">
-						<p>Jl Raya Manyar km 123, Gresik</p>
-					</div>
-					<div class="telp">
-						<p>Telp. (031) 3541552, 3528669 </p>
-					</div>
-					<div class="fax">
-						<p>Fax. (031) 3525585</p>
-					</div>
-					<div class="email">
-						<p>Email. general@angkasarayasteel.com</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-md-60">
-				<div class="content">
-					<p>PT. ANGKASARAYA STEEL - STEEL PIPE - WELDED PIPE - SQUARE PIPE - SURABAYA, INDONESIA. </p>
-				</div>
-				<div class="by">Website design by <a href="https://markdesign.net/">Mark Design</a> Indonesia.</div>
-			</div>
-		</div>
-	</div>
+    <div class="prelative container">
+        <div class="row">
+            <div class="col-md-60">
+                <div class="box-title">
+                    <div class="image"><img src="<?php echo $this->assetBaseurl; ?>opms-logo.png" alt=""></div>
+                    <div class="subtitle">
+                        <p>Rekan terpecaya Anda untuk jual & besi bekas berkualitas. </p>
+                    </div>
+                    <div class="content">
+                        <p>PT. Optima Prima Metal Sinergi - Penjualan pembelian besi & kapal bekas berkualitas. </p>
+                        <p>Website design by <a href="https://markdesign.net/">Mark Design</a> Indonesia.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 </section>
